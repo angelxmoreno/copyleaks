b241005589059e4057d820afce0506635b8fcf03
Angel S. Moreno <angelxmoreno@gmail.com>
2018-05-18 22:37:00 -0400

    updated composer scripts

---
+++ bin/sendTestCoverage.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env ash
+
+
+
+export $(cat .env | grep -v ^# | xargs) && \
+./bin/kahlan --cc=true --reporter=bar --coverage=4 --clover=clover.xml && CODECLIMATE_REPO_TOKEN=${CODECLIMATE_REPO_TOKEN} ./bin/test-reporter --coverage-report=clover.xml
