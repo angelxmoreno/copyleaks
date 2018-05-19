cbbccde1ee6371b78e1196004f2570d18fa36189
Angel S. Moreno <363479+angelxmoreno@users.noreply.github.com>
2018-05-18 23:32:16 -0400

    Travis setup (#2)
    * Added travis
    
    * fix bash/ash
    
    * removed incompatible php envs from travis
    
    * made .env file optional when running tests

--- bin/sendTestCoverage.sh
+++ bin/sendTestCoverage.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env ash
 
-
-
-export $(cat .env | grep -v ^# | xargs) && \
-./bin/kahlan --cc=true --reporter=bar --coverage=4 --clover=clover.xml && CODECLIMATE_REPO_TOKEN=${CODECLIMATE_REPO_TOKEN} ./bin/test-reporter --coverage-report=clover.xml
+#export $(cat .env | grep -v ^# | xargs) && \
+./bin/kahlan --cc=true --reporter=bar --coverage=4 --clover=clover.xml && \
+CODECLIMATE_REPO_TOKEN=${CODECLIMATE_REPO_TOKEN} ./bin/test-reporter --coverage-report=clover.xml
